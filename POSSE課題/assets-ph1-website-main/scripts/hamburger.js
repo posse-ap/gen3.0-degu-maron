22b7ef8eb199b9cb1d9ea41f0036845c7898f355
Ayumi Fujii <ayu0223g@gmail.com>
2022-08-17 23:12:07 +0900

    トップページ・メニューの見た目を整えました

--- POSSE課題/assets-ph1-website-main/scripts/hamburger.js
+++ POSSE課題/assets-ph1-website-main/scripts/hamburger.js
@@ -4,9 +4,40 @@
   const hamburgerBar1 = document.getElementById('js-hamburgerBar1')
   const hamburgerBar2 = document.getElementById('js-hamburgerBar2')
   const menu = document.getElementById('js-menu')
+  const main = document.querySelector('main')
+  const lineSection = document.getElementById('js-lineSection')
+  const footer = document.getElementById('js-footer')
+  const footerLine = document.getElementById('js-footerLine')
+  const footerLineLink = document.getElementById('js-footerLineLink')
+  const footerLogo = document.getElementById('js-footerLogo')
+  const copyRight = document.getElementById('js-copyRight')
+
+  const activeList = [hamburgerBar1, hamburgerBar2, menu]
+  const hideList = [main, lineSection, footerLogo, copyRight]
+  const changeList = [footer, footerLine, footerLineLink]
+
+  function setActive(target) {
+    target.classList.toggle('is-active');
+  }
+  function setHide(target) {
+    target.classList.toggle('hide');
+  }
+  function setChanged(target) {
+    target.classList.toggle('is-changed')
+  }
+
   hamburger.addEventListener('click', () =>{
-    hamburgerBar1.classList.add('is-active')
-    hamburgerBar2.classList.add('is-active')
-    menu.classList.add('is-active')
+    activeList.forEach(target => {
+      setActive(target);
+    });
+    hideList.forEach(target => {
+      setHide(target);
+    });
+    changeList.forEach(target => {
+      setChanged(target)
+    })
   })
 }
+
+// TO DO 
+// ・見た目崩れないように(top, quiz)
