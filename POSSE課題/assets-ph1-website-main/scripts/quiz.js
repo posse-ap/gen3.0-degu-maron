d7b4ca6343ccebd41eff017d0ad4e46528fec1e9
Ayumi Fujii <ayu0223g@gmail.com>
2022-08-19 08:46:25 +0900

    クイズページの見た目を整えた

--- POSSE課題/assets-ph1-website-main/scripts/quiz.js
+++ POSSE課題/assets-ph1-website-main/scripts/quiz.js
@@ -30,19 +30,19 @@
 
   const allQuiz = document.querySelectorAll('.js-quiz');
 
-  const setDisabled = answers => {
+  function setDisabled(answers) {
     answers.forEach(answer => {
       answer.disabled = true;
-    })
-  };
+    });
+  }
 
-  const setTitle = (target, isCorrect) => {
+  function setTitle(target, isCorrect) {
     target.innerText = isCorrect ? '正解！' : '不正解...';
-  };
+  }
 
-  const setClassName = (target, isCorrect) => {
-    target.classList.add(isCorrect ? 'is-correct' : 'is-incorrect')
-  };
+  function setClassName(target, isCorrect) {
+    target.classList.add(isCorrect ? 'is-correct' : 'is-incorrect');
+  }
 
   allQuiz.forEach(quiz => {
     const answers = quiz.querySelectorAll('.js-answer');
